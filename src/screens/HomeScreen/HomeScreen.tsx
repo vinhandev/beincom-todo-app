e06df0998ef5931035057554b3d7afd6b76ff3f9
vinhandev <vinhan.dev@gmail.com>
2024-08-01 22:07:09 +0700

    Add (Animation) initial animation

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,30 @@
+import { useEffect } from "react"
 import { Text, View } from "react-native"
+
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated"
 
 import { useTheme } from "@/theme"
 
 export default function HomeScreen() {
   const { fonts } = useTheme()
-  console.log(fonts.family_400, fonts.family_700)
+
+  const animatedValue = useSharedValue(0)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: animatedValue.value,
+    }
+  })
+
+  useEffect(() => {
+    animatedValue.value = withTiming(1, { duration: 1000 })
+  }, [])
+
   return (
     <View>
-      <Text style={[fonts.size_32, fonts.family_700, fonts.black]}>HomeScreen</Text>
+      <Animated.Text style={[fonts.size_32, fonts.family_700, fonts.black, animatedStyle]}>
+        HomeScreen
+      </Animated.Text>
     </View>
   )
 }
