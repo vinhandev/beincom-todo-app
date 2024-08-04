d2cc5fc00225f1b54cbf805a093728d5c9cfb3ac
vinhandev <vinhan.dev@gmail.com>
2024-08-04 17:25:07 +0700

    Add (Task) delete task feature

--- src/screens/TaskDetailScreen/TaskDetailScreen.tsx
+++ src/screens/TaskDetailScreen/TaskDetailScreen.tsx
@@ -1,16 +1,83 @@
-import { Text, View } from "react-native"
+import { useRef, useState } from "react"
+import { Button, Keyboard, Text, TextInput, View } from "react-native"
 
-import { Q } from "@nozbe/watermelondb"
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet"
 import { withObservables } from "@nozbe/watermelondb/react"
-import { useRoute } from "@react-navigation/native"
+import { useNavigation, useRoute } from "@react-navigation/native"
 
 import Task from "@/models/task.model"
-import { TaskDB } from "@/services/queries/task"
+import { navigationRef } from "@/navigators/Application"
+import { TaskDB, useDeleteTask, useUpdateTask } from "@/services/queries/task"
 
 function TaskItem({ task }: { task: Task }) {
+  const [taskName, setTaskName] = useState(task.title)
+  const bottomSheetRef = useRef<BottomSheet>(null)
+  const { mutate: updateTask } = useUpdateTask()
+  const { mutate: deleteTask } = useDeleteTask()
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand()
+  }
+  function handleClose() {
+    bottomSheetRef.current?.close()
+  }
+
+  async function handleRename() {
+    await updateTask(
+      {
+        id: task.id,
+        title: taskName,
+        isCompleted: task.is_completed,
+        createdAt: task.created_at,
+        categoryId: task.category.id,
+        category: task.category,
+      },
+      {
+        onSuccess: () => {
+          console.log("success task")
+          Keyboard.dismiss()
+          bottomSheetRef.current?.close()
+        },
+        onError: () => {
+          console.log("error")
+        },
+      },
+    )
+  }
+
+  async function handleDelete() {
+    await deleteTask(task.id, {
+      onSuccess: () => {
+        console.log("success task")
+        Keyboard.dismiss()
+        if (navigationRef.canGoBack()) {
+          navigationRef.goBack()
+        }
+      },
+      onError: (error) => {
+        console.error(error)
+      },
+    })
+  }
+
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+      <Text>Name</Text>
       <Text>{task.title}</Text>
+      <Button title="Rename Task" onPress={handleOpen} />
+      <Button title="Delete" onPress={handleDelete} />
+      <BottomSheet
+        enablePanDownToClose
+        index={-1}
+        snapPoints={["50%"]}
+        onClose={handleClose}
+        ref={bottomSheetRef}
+      >
+        <BottomSheetView style={{ flex: 1 }}>
+          <TextInput value={taskName} onChangeText={setTaskName} />
+          <Button title="Rename" onPress={handleRename} />
+        </BottomSheetView>
+      </BottomSheet>
     </View>
   )
 }
@@ -22,10 +89,12 @@ const enhance = withObservables([], ({ taskId }: { taskId: string }) => ({
 const TaskDetail = enhance(TaskItem)
 
 export default function TaskDetailScreen() {
+  const navigation = useNavigation()
   const { taskId } = useRoute().params as { taskId: string }
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+      <Button title="Back" onPress={() => navigation.goBack()} />
       <TaskDetail taskId={taskId} />
     </View>
   )
